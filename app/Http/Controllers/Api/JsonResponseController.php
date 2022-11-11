9a24e012382088cbac2182ab98ef153d2015466d
Julius Macharia <mjulius05@gmail.com>
2022-11-11 12:20:38 +0300

    Add a JSON Response Controller to force JSON Responses and standardize success and error responses from API

---
+++ app/Http/Controllers/Api/JsonResponseController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class JsonResponseController extends Controller
+{
+    /**
+     * @description the sendResponse method provides an easy way to force json response from the api
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendResponse($result, $message){
+        $response = [
+            'success' => true,
+            'data'    => $result,
+            'message' => $message,
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    /**
+     * @description the sendError method allows standard api error responses.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendError($error, $errorMessages = [], $code = 404)
+    {
+    	$response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+        if(!empty($errorMessages)){
+            $response['data'] = $errorMessages;
+        }
+        return response()->json($response, $code);
+
+    }
+}
